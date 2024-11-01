4864b5fba4df75cf45bec52eadd5ba725786abce
Samuel dsp10 <samdsp100@gmail.com>
2024-11-01 12:32:25 -0300

    Algumas mudanças nos arquivos

--- teste/teste.js
+++ teste/teste.js
@@ -1,13 +1,1 @@
-function toggleMessage() {
-    const message = document.getElementById("message");
-    const arrow = document.querySelector(".arrow");
-    
-    if (message.style.display === "none" || message.style.display === "") {
-      message.style.display = "block";
-      arrow.style.transform = "rotate(90deg)";
-    } else {
-      message.style.display = "none";
-      arrow.style.transform = "rotate(0deg)";
-    }
-  }
-  
+// Arquivo de teste JavaScript
